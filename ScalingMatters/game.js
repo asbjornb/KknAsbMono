72aaa74461f6a809fd3dcacaf867771b27c0defb
ABrandt <asbjoernbrandt@gmail.com>
2020-06-23 22:28:12 +0200

    First very basic version, with a button to buy, a display and a loop

---
+++ ScalingMatters/game.js
@@ -0,0 +1,86 @@
+var game = {};
+var player = {};
+var leveldata = {};
+//todo + move long static data to separate referenced file
+// leveldata = [
+//     {
+//       name: "hamlet",
+//       currency: "deer",
+//       research: "venison",
+//       buildings: [ 
+//         {
+//           name: 'Bloodhound',
+//           income: 1,
+//           cost: 10
+//         }, {
+//           name: 'Spearman',
+//           income: 2,
+//           cost: 50
+//         }, {
+//           name: 'Archer',
+//           income: 5,
+//           cost: 200
+//         }, {
+//           name: 'Tracker',
+//           income: 10,
+//           cost: 1000
+//         }, {
+//           name: 'Woodsman',
+//           income: 20,
+//           cost: 10000
+//         }, {
+//           name: 'Pathfinder',
+//           income: 50,
+//           cost: 20000
+//         }, {
+//           name: 'Scout',
+//           income: 100,
+//           cost: 50000
+//         }, {
+//           name: 'Trapper',
+//           income: 200,
+//           cost: 100000
+//         }, {
+//           name: 'Wellspring',
+//           income: 500,
+//           cost: 200000
+//         }, {
+//           name: 'River',
+//           income: 1000,
+//           cost: 1000000
+//         }
+//       ]
+//     }
+// ];
+
+//todo load save on open
+game.baseInterval = 1000; //1000 miliseconds between updates initially?
+
+player.level = "hamlet"; //Starting at hamlet. This and the following should be generalized using level data
+player.resources = {
+    deer : 10
+};
+player.buildings = {
+    bloodhound : 0
+};
+leveldata.buildings = {
+    bloodhoundBaseCost : 10
+}
+
+$(document).ready(function(){
+    $('#buyBloodhound').click(function(){
+        //todo check first if currency is available
+        player.buildings.bloodhound++;
+        player.resources.deer -= leveldata.buildings.bloodhoundBaseCost*Math.pow(1.05,player.buildings.bloodhound);
+    })
+
+    window.setInterval(function(){
+        update();
+    },game.baseInterval)
+})
+
+function update(){
+    console.log('update');
+    player.deer += player.buildings.bloodhound;
+    $('#deer').text(player.resources.deer);
+}
