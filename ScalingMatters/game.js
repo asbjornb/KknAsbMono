9ca386c14aa3c25c4077a84900df754380954bc5
Karsten Nielsen <karsten@kongensholm.dk>
2020-06-24 01:07:45 +0200

    Expanded income tiers.

--- ScalingMatters/game.js
+++ ScalingMatters/game.js
@@ -58,22 +58,33 @@ game.baseInterval = 1000; //1000 miliseconds between updates initially?
 
 player.level = "hamlet"; //Starting at hamlet. This and the following should be generalized using level data
 player.resources = {
-    deer : 10
+    deer : 10,
+	income : 0
 };
-player.buildings = {
-    bloodhound : 0
-};
+player.buildings = [0, 0];
 leveldata.buildings = {
-    bloodhoundBaseCost : 10
+    baseCost : [10, 50],
+	income : [1, 2]
 }
 
 $(document).ready(function(){
     updateVisuals()
-    $('#buyBloodhound').click(function(){
-        var cost=leveldata.buildings.bloodhoundBaseCost*Math.pow(1.05,player.buildings.bloodhound);
-        if(cost<=player.resources.deer){
-            player.buildings.bloodhound++;
+    $('#buyT1').click(function(){
+        var cost=leveldata.buildings.baseCost[0]*Math.pow(1.05,player.buildings[0]);
+		if(cost<=player.resources.deer){
+//			$('#debug').text('Yeah');
+            player.buildings[0]++;
             player.resources.deer -= cost;
+			player.resources.income += 1;
+            updateVisuals(); //Should this call this function or just manually set the 3 deer visuals since we know they are affected?
+        }
+    })
+    $('#buyT2').click(function(){
+        var cost=leveldata.buildings.baseCost[1]*Math.pow(1.05,player.buildings[1]);
+        if(cost<=player.resources.deer){
+            player.buildings[1]++;
+            player.resources.deer -= cost;
+			player.resources.income += 2;
             updateVisuals(); //Should this call this function or just manually set the 3 deer visuals since we know they are affected?
         }
     })
@@ -85,13 +96,17 @@ $(document).ready(function(){
 
 function update(){
     console.log('update');
-    player.resources.deer += player.buildings.bloodhound;
+	for (i=0; i< leveldata.buildings.income.length; i++) {
+		player.resources.deer += player.buildings[i] * leveldata.buildings.income[i];
+	}
     updateVisuals();
 }
 
 function updateVisuals(){
     $('#deer').text(Math.round(player.resources.deer));
-    $('#bloodhounds').text(Math.round(player.buildings.bloodhound));
-    $('#price').text(Math.round(leveldata.buildings.bloodhoundBaseCost*Math.pow(1.05,player.buildings.bloodhound)));
-            
+	$('#income').text(player.resources.income);
+    $('#t1').text(Math.round(player.buildings[0]));
+	$('#t2').text(Math.round(player.buildings[1]));
+    $('#priceT1').text(Math.round(leveldata.buildings.baseCost[0]*Math.pow(1.05,player.buildings[0])));
+	$('#priceT2').text(Math.round(leveldata.buildings.baseCost[1]*Math.pow(1.05,player.buildings[1])));
 }
